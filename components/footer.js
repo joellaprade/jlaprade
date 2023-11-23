248b565bcb11b91c419ed72dccafa1acbd44037d
joellaprade <145079600+joellaprade@users.noreply.github.com>
2023-11-23 08:15:18 -0600

    bg

--- components/footer.js
+++ components/footer.js
@@ -1,18 +1,18 @@
 document.querySelector('#footer').innerHTML += 
 `
-<img src="/assets/footer-background.png" alt="" class="background">
-<div class="copy">
-    <div class="navegate">
-        <b>Navegar</b>
-        <a class="secondary" href="#">Proyectos</a>
-        <a class="secondary" href="#">Productos</a>
-        <a class="secondary" href="#">Cotizar</a>
+    <img src="/assets/footer-background.png" alt="" class="background">
+    <div class="copy">
+        <div class="navegate">
+            <b>Navegar</b>
+            <a class="secondary" href="/proyects/proyects.html">Proyectos</a>
+            <a class="secondary" href="/products/products.html">Productos</a>
+            <a class="secondary" href="https://wa.me/50687083439">Cotizar</a>
+        </div>
+        <div class="contact">
+            <b>Contacto</b>
+            <a  class="secondary" href="https://wa.me/50687083439">cell</a>
+            <a  class="secondary" href="mailto:joellaprade@jlaprade.com">mail</a>
+        </div>
     </div>
-    <div class="contact">
-        <b>Contacto</b>
-        <a  class="secondary" href="#">cell</a>
-        <a  class="secondary" href="#">mail</a>
-    </div>
-</div>
-<p class="secondary">Todos los derechos reservados JLaprade 2023</p>
+    <p class="secondary">Todos los derechos reservados JLaprade 2023</p>
 `
