4e07813beffdbb538516c6f76fe121678db12d2a
joellaprade <145079600+joellaprade@users.noreply.github.com>
2023-12-03 16:33:38 -0600

    avanze copy

--- components/footer.js
+++ components/footer.js
@@ -4,19 +4,19 @@ document.querySelector('#footer').innerHTML +=
     <div class="copy">
         <div class="navegate">
             <b>Navegar</b>
-            <a class="secondary" href="/index.html">Inicio</a>
-            <a class="secondary" href="/products/products.html">Productos</a>
-            <a class="secondary" href="/proyects/proyects.html">Proyectos</a>
-            <a class="secondary" target="_blank" href="https://wa.me/50687083439">Cotizar</a>
+            <a class="secondary hover-float" href="/index.html">Inicio</a>
+            <a class="secondary hover-float" href="/products/products.html">Productos</a>
+            <a class="secondary hover-float" href="/proyects/proyects.html">Proyectos</a>
+            <a class="secondary hover-float" target="_blank" href="https://wa.me/50687083439">Cotizar</a>
         </div>
         <div class="contact">
             <b>Contacto</b>
-            <a class="secondary" target='_blank' href="https://wa.me/50687083439">
+            <a class="secondary hover-float" target='_blank' href="https://wa.me/50687083439">
                 <svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path class="phone" d="M18.3433 20.7053L18.6749 20.3806C20.6292 18.4664 23.7695 18.4786 25.7088 20.4079L27.1235 21.8153C28.2922 22.978 28.2922 24.863 27.1235 26.0257M8.55859 10.8738L8.8754 10.5537C10.8048 8.60396 10.7924 5.4711 8.8478 3.53647L6.99901 1.69719C6.06462 0.767604 4.54968 0.767604 3.61529 1.69719M18.4603 20.6473C18.4603 20.6473 17.112 21.9598 12.1232 16.9967C7.1345 12.0336 8.57175 10.8097 8.57175 10.8097M27.6596 25.535C27.6596 25.535 19.7789 33.3051 7.66605 21.2545C-4.44682 9.20395 3.64977 1.6491 3.64977 1.6491" stroke="white" stroke-width="2"/>
                 </svg>     
             </a>
-            <a class="secondary" target='_blank' href="mailto:joellaprade@jlaprade.com">
+            <a class="secondary hover-float" target='_blank' href="mailto:joellaprade@jlaprade.com">
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="19" viewBox="0 0 32 19" fill="none">
                 <path class="mail" fill-rule="evenodd" clip-rule="evenodd" d="M2 0C0.895431 0 0 0.895431 0 2V17C0 18.1046 0.895432 19 2 19H29.92C31.0246 19 31.92 18.1046 31.92 17V2C31.92 0.895431 31.0246 0 29.92 0H2ZM28.2406 2H3.74726L15.9615 10.1968L28.2406 2ZM29.92 3.2836L16.5321 12.2206C16.3631 12.3383 16.1624 12.4001 15.9593 12.4002C15.8092 12.4002 15.6578 12.3665 15.517 12.2967C15.4717 12.2744 15.4279 12.2485 15.3861 12.2192L2 3.23605V17H29.92L29.92 3.2836Z" fill="white"/>
                 </svg>
